73ebcf375a8b76c91567a87f4874708fe262f9ef
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2023-05-19 21:42:51 +0200

    added cli

--- app/src/cli/crossPlatformSpecs-cli.ts
+++ app/src/cli/crossPlatformSpecs-cli.ts
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
-import crossPlatformSpecs from "../crossPlatformSpecs.js"
+import * as spec from "../crossPlatformSpecs.js"
 import { program } from "commander"
 import reqPackageJson, { reqPackagePath } from "req-package-json"
 import {promises as fs} from "fs"
 import * as path from "path"
 const config = reqPackageJson()
 import * as console from "./../lib/logger"
+
 
 program
   .version(config.version)
@@ -14,12 +15,12 @@ program
   .option('-s, --silent', 'silence stdout')
   // .argument('<required example>', "description of required example")
   // .argument('[optional example]', "description of optional example")
-  .action((arg1, arg2, options) => {
+  .argument('[specific spec]', `Optionally provide a spec name of interest to only return that spec. Can be one of ["environment", "osName", "osVersion", "runtime", "runtimeVersion", "engine", "engineVersion", "cpuName", "cpuCores", "cpuArch", "memAvailable", "memFree"]. If omitted, a summery of all specs will be returned.`)
+  .action((spec, options) => {
     console.setVerbose(!options.silent)
     
-    // crossPlatformSpecs()
-    
-    
+    if (spec !== undefined) console.log(spec[spec])
+    else console.log(spec.allSpecs())
   })
 
 .parse(process.argv)
