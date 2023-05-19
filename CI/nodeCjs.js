1ed1014a8c8c0572e5988f0600f738613f170465
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2023-05-19 22:32:05 +0200

    update ci to test different envs

---
+++ CI/nodeCjs.js
@@ -0,0 +1,5 @@
+const spec = require("cross-platform-specs")
+
+console.log(spec.default())
+console.log()
+console.log(spec)
---
+++ CI/nodeEsm.mjs
@@ -0,0 +1,6 @@
+import * as spec from "cross-platform-specs"
+import all from "cross-platform-specs"
+
+console.log(all())
+console.log()
+console.log(spec)
