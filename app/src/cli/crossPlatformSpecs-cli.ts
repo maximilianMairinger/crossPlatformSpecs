f3c3093b48f4db2fe4cfcc285ce3ffe4f04a12f5
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2023-05-22 18:39:23 +0200

    rm silent option cli

--- app/src/cli/crossPlatformSpecs-cli.ts
+++ app/src/cli/crossPlatformSpecs-cli.ts
@@ -5,19 +5,19 @@ import reqPackageJson, { reqPackagePath } from "req-package-json"
 import {promises as fs} from "fs"
 import * as path from "path"
 const config = reqPackageJson()
-import * as console from "./../lib/logger"
+// import * as console from "./../lib/logger"
 
 
 program
   .version(config.version)
   .description(config.description)
   .name(config.name)
-  .option('-s, --silent', 'silence stdout')
+  // .option('-s, --silent', 'silence stdout')
   // .argument('<required example>', "description of required example")
   // .argument('[optional example]', "description of optional example")
   .argument('[specific spec]', `Optionally provide a spec name of interest to only return that spec. Can be one of ["environment", "osName", "osVersion", "runtime", "runtimeVersion", "engine", "engineVersion", "cpuName", "cpuCores", "cpuArch", "memAvailable", "memFree"]. If omitted, a summery of all specs will be returned.`)
   .action((spec, options) => {
-    console.setVerbose(!options.silent)
+    // console.setVerbose(!options.silent)
     
     if (spec !== undefined) console.log(spec[spec])
     else console.log(spec.allSpecs())
